9a6820181656c717acfd8d5119e757ea97c2c0cb
clooney <phpjd@qq.com>
2017-06-19 11:54:45 +0800

    Create La Poste tracking API
    Create La Poste tracking API

---
+++ detect-a-carrier-by-tracking-code.php
@@ -0,0 +1,7 @@
+<?php
+
+include("track.class.php");
+$track = new Trackingmore;
+$trackingNumber = 'RK469499309FR';
+$track = detectCarrier($trackingNumber);
+?>
---
+++ get-tracking-results-of-a-single-tracking.php
@@ -0,0 +1,7 @@
+<?php
+
+include("track.class.php");
+$track = new Trackingmore;
+
+$track = $track->getSingleTrackingResult('laposte','RK152199309FR');
+?>
